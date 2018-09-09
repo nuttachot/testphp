7925da8e93436a53c60d3e24f23d1e1f1c1da511
Nuttachot Promrit <nuttachotpromrit@EXPLORER.local>
2018-09-09 13:54:42 +0700

    first commit

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ footer {
   </nav>
   
   <article>
-    <h1>ทำไมต้อง Run นะ</h1>
+    <h1>ทำไมต้อง Run นะนะ</h1>
     <img src="img/codie_small.png" width="75" height="120">
     <p>กขคง กขคงกขคงกขคงกขคงกขคงกขคงกขคงกขคงกขคง  กขคงกขคงกขคงกขคง  กขคงกขคงกขคงกขคง   </p>
     <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
